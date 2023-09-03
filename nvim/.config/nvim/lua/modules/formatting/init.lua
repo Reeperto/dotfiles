f44f655876d1037b0afe52a4367300064566f57f
Eli Griffiths <elimgriffiths1@gmail.com>
2023-09-03 15:40:19 -0700

    Nvim: Clang Formatting

---
+++ nvim/.config/nvim/lua/modules/formatting/init.lua
@@ -0,0 +1,34 @@
+local servers = {}
+
+return {
+	{
+		"mhartington/formatter.nvim",
+		config = function()
+			local util = require("formatter.util")
+
+			servers.lua = require("formatter.filetypes.lua").stylua
+			servers.c = function()
+				return {
+					exe = "clang-format",
+					args = {
+						"-assume-filename",
+						util.escape_path(util.get_current_buffer_file_name()),
+						-- "--style=" .. "file:" .. util.escape_path(
+						-- 	vim.fn.stdpath("config") .. "/lua/modules/formatting/.clang-format"
+						-- ),
+					},
+					stdin = true,
+					try_node_modules = true,
+				}
+			end
+			servers.cpp = servers.c
+			servers.objc = servers.c
+
+			require("formatter").setup({
+				logging = true,
+				log_level = vim.log.levels.ERROR,
+				filetype = servers,
+			})
+		end,
+	},
+}
