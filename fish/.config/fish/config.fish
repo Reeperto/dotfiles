c271f930c4884cc1bb264e28e8f9fd56dbd6e2e5
Eli Griffiths <elimgriffiths1@gmail.com>
2024-01-15 03:48:34 -0800

    Linux Target Changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,10 @@ end
 
 mise activate | source
 eval (luarocks path)
+
+# pnpm
+set -gx PNPM_HOME "/home/eeleyes/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
