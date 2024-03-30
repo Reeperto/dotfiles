b739b06f86725151199e48ad015651d1b2a214b2
Eli Griffiths <elimgriffiths1@gmail.com>
2024-03-30 05:12:47 -0700

    Fix Luarocks in Fish and Mise

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,5 @@
+mise activate fish | source
+
 if status is-interactive
 end
 
@@ -5,21 +7,21 @@ if test (tty) = /dev/tty1
     exec Hyprland
 end
 
-mise activate | source
-eval (luarocks path)
+# eval (mise x luajit --command "luarocks path") <-- this hangs for some reason :3
+eval (mise x luajit -- luarocks path)
 
 switch (uname)
     case Linux
         set LUA_CPATH "$LUA_CPATH;$HOME/.lua/lib/linux/?.so"
-        set LUA_PATH  "$LUA_PATH;$HOME/.lua/lib/linux/?.lua;$HOME/.lua/lib/linux/?/init.lua"
+        set LUA_PATH "$LUA_PATH;$HOME/.lua/lib/linux/?.lua;$HOME/.lua/lib/linux/?/init.lua"
     case Darwin
         set LUA_CPATH "$LUA_CPATH;$HOME/.lua/lib/macos/?.so"
-        set LUA_PATH  "$LUA_PATH;$HOME/.lua/lib/macos/?.lua;$HOME/.lua/lib/linux/?/init.lua"
+        set LUA_PATH "$LUA_PATH;$HOME/.lua/lib/macos/?.lua;$HOME/.lua/lib/linux/?/init.lua"
 end
 
 # pnpm
 set -gx PNPM_HOME "/home/eeleyes/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
--- fish/.config/fish/functions/notes.fish
+++ fish/.config/fish/functions/notes.fish
@@ -1,8 +1,8 @@
 function notes
-	set file (fd -H "notes\.tex" ~/school/ | sk)
+    set file (fd -H "notes\.tex" ~/school/ | sk)
 
-	if test -n "$file"
-		cd (path dirname $file)
-		nvim $file
-	end
+    if test -n "$file"
+        cd (path dirname $file)
+        nvim $file
+    end
 end
