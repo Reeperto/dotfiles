150c40ae2d974fc66a0e62fc762626923a649fa8
Eli Griffiths <elimgriffiths1@gmail.com>
2024-05-16 13:42:31 -0700

    Fish: Cleanup

--- fish/.config/fish/functions/br.fish
+++
@@ -1,1 +0,0 @@
-/Users/reeperto/Library/Application Support/org.dystroy.broot/launcher/fish/br.fish
--- fish/.config/fish/functions/gt.fish
+++ fish/.config/fish/functions/gt.fish
@@ -1,4 +1,3 @@
-function gt --wraps='fd -t d | sk' --wraps='cd (fd -t d | sk)' --wraps='cd (fd -t d ~ | sk)' --wraps='cd (fd -t d . ~ | sk)' --wraps='cd (fd --type directory . ~ | sk)' --description 'alias gt cd (fd --type directory . ~ | sk)'
+function gt
   cd (fd --type directory . ~ | sk) $argv
-        
 end
