faaf491abb7de5a8555a6db915dcf26b7497a345
Eli Griffiths <elimgriffiths1@gmail.com>
2025-03-03 23:59:23 -0800

    Update wezterm mappings

--- wezterm/.config/wezterm/mac_keys.lua
+++ wezterm/.config/wezterm/mac_keys.lua
@@ -33,38 +33,42 @@ for trigger, _action in pairs(events) do
 end
 
 return {
-	mappings = {
-		{ key = "]",     mods = "CMD|SHIFT", action = act.ActivateTabRelative(1) },
-		{ key = "[",     mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1) },
-		{ key = "Enter", mods = "CMD",       action = act.ToggleFullScreen },
-		-- { key = "Enter", mods = "CMD",       action = act.ActivateCopyMode },
-		{ key = "R",     mods = "CMD|SHIFT", action = act.ReloadConfiguration },
-		{ key = "+",     mods = "CMD",       action = act.IncreaseFontSize },
-		{ key = "-",     mods = "CMD",       action = act.DecreaseFontSize },
-		{ key = "0",     mods = "CMD",       action = act.ResetFontSize },
-		{ key = "c",     mods = "CMD",       action = act.CopyTo("Clipboard") },
-		{ key = "n",     mods = "CMD",       action = act.SpawnWindow },
-		{
-			key = "P",
-			mods = "CMD|SHIFT",
-			action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
-		},
-		{ key = "v",          mods = "CMD", action = act.PasteFrom("Clipboard") },
-		{ key = "LeftArrow",  mods = "CMD", action = act.ActivatePaneDirection("Left") },
-		{ key = "RightArrow", mods = "CMD", action = act.ActivatePaneDirection("Right") },
-		{ key = "UpArrow",    mods = "CMD", action = act.ActivatePaneDirection("Up") },
-		{ key = "DownArrow",  mods = "CMD", action = act.ActivatePaneDirection("Down") },
-		{ key = "f",          mods = "CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "d",          mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "t",          mods = "CMD", action = act.SpawnTab("CurrentPaneDomain") },
-		{ key = "w",          mods = "CMD", action = act.CloseCurrentTab({ confirm = false }) },
-		{ key = "c",          mods = "ALT", action = act.EmitEvent('trigger-search-zotero') },
-		{ key = "n",          mods = "ALT", action = act.EmitEvent('trigger-search-notes') },
-		{ key = "v",          mods = "ALT", action = act.EmitEvent('trigger-fd-to-nvim') },
-	},
-	leader = {
-		key = "b",
-		mods = "CMD",
-		timeout_milliseconds = 2000
-	}
+    mappings = {
+        { key = "]",     mods = "CMD|SHIFT", action = act.ActivateTabRelative(1) },
+        { key = "[",     mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1) },
+
+        { key = "<",     mods = "CMD|SHIFT", action = act.MoveTabRelative(-1) },
+        { key = ">",     mods = "CMD|SHIFT", action = act.MoveTabRelative(1) },
+
+        { key = "Enter", mods = "CMD",       action = act.ToggleFullScreen },
+        -- { key = "Enter", mods = "CMD",       action = act.ActivateCopyMode },
+        { key = "R",     mods = "CMD|SHIFT", action = act.ReloadConfiguration },
+        { key = "+",     mods = "CMD",       action = act.IncreaseFontSize },
+        { key = "-",     mods = "CMD",       action = act.DecreaseFontSize },
+        { key = "0",     mods = "CMD",       action = act.ResetFontSize },
+        { key = "c",     mods = "CMD",       action = act.CopyTo("Clipboard") },
+        { key = "n",     mods = "CMD",       action = act.SpawnWindow },
+        {
+            key = "P",
+            mods = "CMD|SHIFT",
+            action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
+        },
+        { key = "v",          mods = "CMD", action = act.PasteFrom("Clipboard") },
+        { key = "LeftArrow",  mods = "CMD", action = act.ActivatePaneDirection("Left") },
+        { key = "RightArrow", mods = "CMD", action = act.ActivatePaneDirection("Right") },
+        { key = "UpArrow",    mods = "CMD", action = act.ActivatePaneDirection("Up") },
+        { key = "DownArrow",  mods = "CMD", action = act.ActivatePaneDirection("Down") },
+        { key = "f",          mods = "CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+        { key = "d",          mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+        { key = "t",          mods = "CMD", action = act.SpawnTab("CurrentPaneDomain") },
+        { key = "w",          mods = "CMD", action = act.CloseCurrentTab({ confirm = false }) },
+        { key = "c",          mods = "ALT", action = act.EmitEvent('trigger-search-zotero') },
+        { key = "n",          mods = "ALT", action = act.EmitEvent('trigger-search-notes') },
+        { key = "v",          mods = "ALT", action = act.EmitEvent('trigger-fd-to-nvim') },
+    },
+    leader = {
+        key = "b",
+        mods = "CMD",
+        timeout_milliseconds = 2000
+    }
 }
