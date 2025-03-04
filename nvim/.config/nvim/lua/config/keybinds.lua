d096640ddbaeb708dd3c5aaf6b83cf40ba138cbe
Eli Griffiths <elimgriffiths1@gmail.com>
2025-03-04 00:03:27 -0800

    Clean Slate

--- nvim/.config/nvim/lua/config/keybinds.lua
+++
@@ -1,9 +0,0 @@
-local set = vim.keymap.set
-
-set("n", "|", vim.cmd.vsplit, {})
-set("n", "\\", vim.cmd.split, {})
-
-set("n", "<leader>q", vim.cmd.quit, {})
-set("n", "<leader>Q", Utils.func.delay(vim.cmd, [[:q!]]), {})
-set("n", "<leader>w", vim.cmd.write, {})
-set("n", "<leader>m", vim.cmd.make, {})
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "EdenEast/nightfox.nvim",
-    "kvrohit/substrata.nvim",
-    "nyoom-engineering/oxocarbon.nvim",
-    "nvimdev/zephyr-nvim",
-    "AlexvZyl/nordic.nvim"
-}
