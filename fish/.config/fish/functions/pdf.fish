8a4935ad1c9201de7b95c89005aaced365a2f6f9
Eli Griffiths <elimgriffiths1@gmail.com>
2025-03-03 23:58:15 -0800

    OS Specific PDF Command

--- fish/.config/fish/functions/pdf.fish
+++ fish/.config/fish/functions/pdf.fish
@@ -1,6 +1,11 @@
 function pdf
     set file (fd -H ".*\.pdf" | sk)
     if test -n "$file"
-        zathura "$file" 2>&1 >/dev/null & disown
+        switch (uname)
+            case Linux
+                zathura "$file" 2>&1 >/dev/null & disown
+            case Darwin
+                sioyek "$file" 2>&1 >/dev/null & disown
+        end
     end
 end
